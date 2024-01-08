2024398085486bff4f86ffd797d518e5636bc9c0
deadliest7 <jafardahod7@gmail.com>
2024-01-08 13:17:02 +0530

    'part'

--- App.js
+++ App.js
@@ -16,6 +16,6 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: 'blue',
     alignItems: 'center',
-    justifyContent: 'Can we do it again??',
+    justifyContent: 'part 2',
   },
 });
